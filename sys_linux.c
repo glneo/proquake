3984dcf87db97ae31385aa57dcf61f30095428be
Andrew F. Davis <glneolistmail@gmail.com>
2015-01-08 12:30:24 -0600

    Fix

--- sys_linux.c
+++ sys_linux.c
@@ -453,7 +453,7 @@ int main (int c, char **v)
 		nostdout = 1;
 	else {
 		fcntl(0, F_SETFL, fcntl (0, F_GETFL, 0) | FNDELAY);
-		printf("Linux Quake -- Version %0.3f\n", PROQUAKE_VERSION);
+		printf("Linux Quake -- Version %0.3f\n", ENGINE_VERSION);
 	}
 
     oldtime = Sys_FloatTime () - 0.1;
